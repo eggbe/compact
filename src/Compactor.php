754a682851556314ad5b64d859dbb5a97ffbf428
hacpaka <hacpaka@eggbe.com>
2017-02-16 20:50:36 +0300

    +

--- src/Compactor.php
+++ src/Compactor.php
@@ -106,7 +106,7 @@ class Compactor {
 			}
 
 			if ($flags & self::CO_ALLOW_ARRAYABLE && ($data instanceof IArrayable || method_exists($data, 'toArray'))){
-					return [self::DT_OBJECT, get_class($data), self::pack($data->toArray(), $flags)];
+				return [self::DT_OBJECT, get_class($data), self::pack($data->toArray(), $flags)];
 			}
 
 			throw new \Exception('Unpresentable objects of class ' .  get_class($data) . '!');
